5889fc5ef463be36cbd17332f5cc3885328d114e
Tulio Pinto Neto <tulio_neto_2@hotmail.com>
2020-11-19 18:14:22 -0300

    style: Melhorando a cara da tela de Login - Adicionado um logo e um background image.

--- Estudos/lib/login.dart
+++ Estudos/lib/login.dart
@@ -14,58 +14,83 @@ class _LoginState extends State<Login> {
   String email;
   String password;
 
+  Widget _body() {
+    return Container(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Logo
+            Container(
+              width: 100,
+              height: 100,
+              child: Image.asset('images/logo.png'),
+            ),
+            Container(height: 30),
+            // Campo de e-mail
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: TextField(
+                onChanged: (textoDigitado) {
+                  email = textoDigitado;
+                },
+                autofocus: true,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: "E-mail",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            // Campo de password
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: TextField(
+                onChanged: (textoDigitado) {
+                  password = textoDigitado;
+                },
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            // Campo do botão de login
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 30.0),
+              child: RaisedButton(
+                onPressed: () {
+                  if (email == "teste@gmail.com" && password == "123456") {
+                    // Redireciona para Home
+                    Navigator.of(context).pushReplacementNamed('/home');
+                  }
+                },
+                child: Text("Login"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Container(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Campo de e-mail
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 16.0),
-                child: TextField(
-                    onChanged: (textoDigitado) {
-                      email = textoDigitado;
-                    },
-                    autofocus: true,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      labelText: "E-mail",
-                      border: OutlineInputBorder(),
-                    )),
-              ),
-              // Campo de password
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 16.0),
-                child: TextField(
-                    onChanged: (textoDigitado) {
-                      password = textoDigitado;
-                    },
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: "Password",
-                      border: OutlineInputBorder(),
-                    )),
-              ),
-              // Campo do botão de login
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 30.0),
-                child: RaisedButton(
-                  onPressed: () {
-                    if (email == "teste@gmail.com" && password == "123456") {
-                      // Redireciona para Home
-                      Navigator.of(context).pushReplacementNamed('/home');
-                    }
-                  },
-                  child: Text("Login"),
-                ),
-              ),
-            ],
+      child: Stack(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              'images/bg.png',
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
+          Container(color: Colors.white.withOpacity(0.1)),
+          _body(),
+        ],
       ),
     );
   }
