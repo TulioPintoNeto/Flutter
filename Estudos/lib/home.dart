79aa35f1a0c56426ef364378c3c4e4fc3e15f806
Tulio Pinto Neto <tulio_neto_2@hotmail.com>
2020-11-19 22:55:55 -0300

    feat: Drawer e BottomNavigation - Adicionado o botão para menu lateral (drawer - ícone de menu hamburger) e menu de navegação inferior, possui uma variável para navegar mas sem conteúdo em ambos.

--- Estudos/lib/home.dart
+++ Estudos/lib/home.dart
@@ -8,12 +8,55 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       // App bar com o título da rede social
       appBar: AppBar(
-        title: Text("Rede Social"),
+        backgroundColor: Colors.white70,
+        title: Text(
+          "Rede Social",
+          style: TextStyle(
+            color: Colors.black87,
+          ),
+        ),
+        automaticallyImplyLeading: false,
+        iconTheme: IconThemeData(color: Colors.black87),
+      ),
+      endDrawer: Drawer(),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        backgroundColor: Colors.white70,
+        selectedItemColor: Colors.black87,
+        unselectedItemColor: Colors.black38,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "Search",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.camera),
+            label: "Camera",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
       ),
     );
   }
